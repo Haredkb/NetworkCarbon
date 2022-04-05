32534a73499451a59cbdebb179bf7e0f4e1c7672
amhelton <40064179+amhelton@users.noreply.github.com>
2022-04-05 10:09:50 -0400

    Update Analysis_eg.R

--- Analysis_eg.R
+++ Analysis_eg.R
@@ -13,8 +13,22 @@ net <- readRDS("data/eg_watershed.RDS")
 #update igraph object to include calculations
 calc_net <- solveMB(net)
 
-#Extract vertices dataframe for easy viewing 
-v_df <- as.data.frame(get.vertex.attribute(calc_net))
+
+
+
+## Read initial network (starting values)
+
+
+##for loop for each time step
+  #read input files (inputs external to the network)
+    #water yield, doc yield, litter inputs, temperature
+  
+  #solve network function
+  net <- solveMB(net)
+
+  #Extract vertices dataframe for easy viewing (if timestep every 10 time steps write a dataframe) 
+  v_df <- as.data.frame(get.vertex.attribute(calc_net))
+
 
 ##Note the "lengthup_m" is from the original GIS calculations, so is determined seperately from this code. 
 ##Therefore, the SLout and lengthup_m should be ~ the same. 
